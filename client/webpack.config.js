e73e735610d9f627ee5c320f9675e2464b2b4afe
p0ptartz <ericwm420@gmail.com>
2023-03-09 11:58:19 -0500

    another try

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,24 +30,26 @@ module.exports = () => {
       }),
 
       // Creates a manifest.json file.
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'J.A.T.E.',
-        short_name: 'TextEdit',
-        description: 'Edit your text!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        // start_url: './',
-        // publicPath: './',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
+      // new WebpackPwaManifest({
+      //   fingerprints: false,
+      //   inject: true,
+      //   name: 'J.A.T.E.',
+      //   short_name: 'TextEdit',
+      //   description: 'Edit your text!',
+      //   background_color: '#225ca3',
+      //   theme_color: '#225ca3',
+      //   // start_url: './',
+      //   // publicPath: './',
+      //   icons: [
+      //     {
+      //       src: path.resolve('src/images/logo.png'),
+      //       sizes: [96, 128, 192, 256, 384, 512],
+      //       destination: path.join('assets', 'icons'),
+      //     },
+      //   ],
+      // }),
+
+      WebpackPwaManifest()
     ],
 
     module: {
